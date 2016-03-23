f262223fabadcb9d4daa76193ed374662f81b7e2
Lena <seo-xcube@yandex.ru>
2016-03-23 16:57:43 +0400

    finish written function

--- build/js/check.js
+++ build/js/check.js
@@ -11,13 +11,17 @@ function getMessage(a, b){
     return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
   };
   if (Array.isArray(a)) {
-    var sum = a;
-    for (i = 0; i < a.lenght; i++) {
+    var sum = 0;
+    for (i = 0; i < a.length; i++) {
       sum += a[i];
     }
     return "Количество красных точек во всех строчках изображения: " + sum;
   };
   if (Array.isArray(a) && Array.isArray(b)) {
+    var square = 0;
+    for (i = 0; i < a.length; i++) {
+      square = (a[i] * b[i]) + length; // надеюсь я правильно помню матетматику >_>
+    }
     return "Общая площадь артефактов сжатия: " + ((a+b)*(a+b)) + " пикселей";
   };
 };
