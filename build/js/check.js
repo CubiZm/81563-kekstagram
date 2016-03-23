2014914a90a828298415fd70290295877dc6ffbf
Lena <seo-xcube@yandex.ru>
2016-03-23 14:09:15 +0400

    corrected condition

--- build/js/check.js
+++ build/js/check.js
@@ -11,7 +11,11 @@ function getMessage(a, b){
     return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
   };
   if (Array.isArray(a)) {
-    return "Количество красных точек во всех строчках изображения: " (a + b);
+    var sum = a;
+    for (i = 0; i < a.lenght; i++) {
+      sum += a[i];
+    }
+    return "Количество красных точек во всех строчках изображения: " + sum;
   };
   if (Array.isArray(a) && Array.isArray(b)) {
     return "Общая площадь артефактов сжатия: " + ((a+b)*(a+b)) + " пикселей";
