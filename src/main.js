dc0d9c708b7c7c8286842eafb37c3acdb325ffd9
Lena <seo-xcube@yandex.ru>
2016-03-30 11:05:57 +0500

    repaired scripts :tada:

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 require('../src/check');
 require('../src/resizer');
 require ('../src/upload');
-require ('../src/upload-stat.min');
+//require ('./src/upload-stat.min');
 console.log('Hello webpack');
