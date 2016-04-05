9bbb9801a97b2d9b6909eecbfe83b22c94df59cf
CubiZm <seo-xcube@yandex.ru>
2016-04-05 22:52:19 +0400

    Started module4-task1
    Потому что ждать больше не можно, потом волью в нужную ветку и там
    продолжу.

---
+++ src/pictures.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function() {
+  var blockFilters = document.querySelector('.filters');
+  blockFilters.classList.add('hidden');
+
+  var picturesContainer = document.querySelector('.pictures');
+  var templateElement = document.getElementById('picture-template');
+  var elementToClone;
+});
