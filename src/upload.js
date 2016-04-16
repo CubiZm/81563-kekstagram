d8cc3a0ffa391ee2069525b80ba6aa6d49bd463f
CubiZm <seo-xcube@yandex.ru>
2016-04-16 14:33:20 +0400

    Revert "fix travis"
    This reverts commit 63b0861f6a2c8643d45212f8bbbac39046c83408.

--- src/upload.js
+++ src/upload.js
@@ -272,7 +272,7 @@ var browserCookies = require('browser-cookies');
     if (resizeFormIsValid()) {
       currentResizer.setConstraint(+resizeFormX.value, +resizeFormY.value, +resizeFormSide.value);
     }
-  }
+  };
 
 
   /**
