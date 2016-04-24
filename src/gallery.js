1a82c5c49d6238a6dbf1a9daaa17200792cde87d
CubiZm <seo-xcube@yandex.ru>
2016-04-24 17:54:14 +0400

    upd2

--- src/gallery.js
+++ src/gallery.js
@@ -1,73 +1,73 @@
 'use strict';
 
 define('gallery', ['./utils'], function(utils) {
-  // return function(showGallery) {
-    var galleryContainer = document.querySelector('.gallery-overlay');
-    var closeElement = galleryContainer.querySelector('.gallery-overlay-close');
-    var thumbnailsContainer = galleryContainer.querySelector('.gallery-overlay-image');
-    // var preview = galleryContainer.querySelector('.gallery-overlay-preview');
-    var likes = galleryContainer.querySelector('.likes-count');
-    var comments = galleryContainer.querySelector('.comments-count');
-    var keyRightCheck = utils.listenKey(39, switchNextPicture);
-    var keyLeftCheck = utils.listenKey(37, switchPrevPicture);
-    var keyEsc = utils.listenKey(27, closeGallery);
+// return function(showGallery) {
+  var galleryContainer = document.querySelector('.gallery-overlay');
+  var closeElement = galleryContainer.querySelector('.gallery-overlay-close');
+  var thumbnailsContainer = galleryContainer.querySelector('.gallery-overlay-image');
+  // var preview = galleryContainer.querySelector('.gallery-overlay-preview');
+  var likes = galleryContainer.querySelector('.likes-count');
+  var comments = galleryContainer.querySelector('.comments-count');
+  var keyRightCheck = utils.listenKey(39, switchNextPicture);
+  var keyLeftCheck = utils.listenKey(37, switchPrevPicture);
+  var keyEsc = utils.listenKey(27, closeGallery);
 
-    /** @type {Array.<string>} */
-    var galleryPictures = [];
-    /** @type {number} */
-    var activePicture = 0;
-
-
-    var closeGallery = function() {
-      galleryContainer.classList.add('invisible');
-    };
-    closeElement.addEventListener('click', function() {
-      closeGallery();
-    });
-
-    /**
-   * @param {Array.<pictues>} pictures
-   */
-    var showPhoto = function(numberPhoto) {
-      //galleryContainer.classList.remove('invisible');
-      var nextPhoto = galleryPictures[numberPhoto];
-      var pictureElement = new Image();
+  /** @type {Array.<string>} */
+  var galleryPictures = [];
+  /** @type {number} */
+  var activePicture = 0;
 
 
-      pictureElement.onload = function(evt) {
-        thumbnailsContainer.src = evt.target.src;
-        thumbnailsContainer.alt = nextPhoto.date;
-        likes.textContent = nextPhoto.likes;
-        comments.textContent = nextPhoto.comments;
-      };
+  var closeGallery = function() {
+    galleryContainer.classList.add('invisible');
+  };
+  closeElement.addEventListener('click', function() {
+    closeGallery();
+  });
 
-      pictureElement.onerror = function() {
-        showPhoto(activePicture + 1);
-      };
-    //pictureElement.src = nextPhoto.url;
+  /**
+ * @param {Array.<pictues>} pictures
+ */
+  var showPhoto = function(numberPhoto) {
+    //galleryContainer.classList.remove('invisible');
+    var nextPhoto = galleryPictures[numberPhoto];
+    var pictureElement = new Image();
+
+
+    pictureElement.onload = function(evt) {
+      thumbnailsContainer.src = evt.target.src;
+      thumbnailsContainer.alt = nextPhoto.date;
+      likes.textContent = nextPhoto.likes;
+      comments.textContent = nextPhoto.comments;
     };
 
-    window.addEventListener('keydown', function(evt) {
-      if (!galleryContainer.classList.contains('invisible') && keyEsc) {
-        evt.preventDefault();
-        closeGallery();
-      }
-    });
-
-    function switchNextPicture() {
+    pictureElement.onerror = function() {
       showPhoto(activePicture + 1);
+    };
+  //pictureElement.src = nextPhoto.url;
+  };
+
+  window.addEventListener('keydown', function(evt) {
+    if (!galleryContainer.classList.contains('invisible') && keyEsc) {
+      evt.preventDefault();
+      closeGallery();
     }
+  });
 
-    function switchPrevPicture() {
-      showPhoto(activePicture - 1);
-    }
+  function switchNextPicture() {
+    showPhoto(activePicture + 1);
+  }
 
-    window.addEventListener('keydown', keyRightCheck);
-    thumbnailsContainer.addEventListener('click', switchNextPicture);
+  function switchPrevPicture() {
+    showPhoto(activePicture - 1);
+  }
 
-    window.addEventListener('keydown', keyLeftCheck);
-    // prevPicture.addEventListener('click', switchPrevPicture);
-    showPhoto(activePicture);
+  window.addEventListener('keydown', keyRightCheck);
+  thumbnailsContainer.addEventListener('click', switchNextPicture);
+
+  window.addEventListener('keydown', keyLeftCheck);
+  // prevPicture.addEventListener('click', switchPrevPicture);
+  showPhoto(activePicture);
   // };
 });
 
