b1f952f568cf0bc447f8f187f04daf72e061e44f
CubiZm <seo-xcube@yandex.ru>
2016-04-30 13:58:39 +0400

    upd main.js for AMD

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
 //require('./check');
-require('./resizer');
-require('./upload');
-require('./pictures');
+// require('./resizer');
+// require('./upload');
+// require('./pictures');
 //require('./upload-stat.min');
+require([
+  './resizer',
+  './upload',
+  './pictures'
+])
