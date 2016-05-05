ae2cc91993a343527d33c733cb3c6763cf558c51
Lena <seo-xcube@yandex.ru>
2016-05-05 14:32:07 +0500

    upd

--- src/pictures/base-component.js
+++ src/pictures/base-component.js
@@ -2,12 +2,18 @@
 define(['./render-photo'], function(getPictureElement) {
   var BaseComponent = function(data, container) {
     this.data = data;
+    // NB! getPictureElement is not a function -- НАЙДИ ПРОБЛЕМУ (и ваще это переменная)
     this.element = getPictureElement(data, container);
     this.onPhotoListClick = this.onPhotoListClick.bind(this);
 
     this.element.addEventListener('click', this.onPhotoListClick);
     container.appendChild(this.element);
   }
+
+  // NB!  Вот тут подумай как разберешься с функцией
+  BaseComponent.prototype.getPictureElement = function(data) {
+    return data.element;
+  };
   BaseComponent.prototype.remove = function() {
     this.element.removeEventListener('click', this.onClick);
     this.element.parentNode.removeChild(this.element);
